8b63d104b2e0de9216ea9bce6fc85494bbd56021
Abbas Pirmoradi <abbaspirmorady@gmail.com>
2025-06-09 10:30:49 -0600

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,10 @@ import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 import RegisterStudent from './components/RegisterStudent';
 import StudentList from './components/StudentList';
 import './App.css';
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
 
 function App() {
     return (
